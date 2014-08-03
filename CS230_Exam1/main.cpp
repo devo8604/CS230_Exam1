8a67235062a06be54bd66fb7e69daaf11faa50d0
devo8604 <devo8604@gmail.com>
2014-08-03 07:16:35 -0600

    Initial Commit

---
+++ CS230_Exam1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CS230_Exam1
+//
+//  Created by Devon Smith on 8/3/14.
+//  Copyright (c) 2014 Devon Smith. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
